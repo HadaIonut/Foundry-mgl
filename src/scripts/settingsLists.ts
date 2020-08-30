1f9d2f967489a9cd48f477e0d26b420141d3c230
Hada Ionut <hadaionut@gmail.com>
2020-08-30 20:24:07 +0300

    Solved an error with the JSON.parse method.

--- src/scripts/settingsLists.ts
+++ src/scripts/settingsLists.ts
@@ -4,7 +4,7 @@ export default {
         key: "conversionMultipliers",
         data: {
             type: String,
-            default: "{inch: 2.5,feet: 0.3,mile: 1.6,pound: 0.5}",
+            default: "{\"inch\": 2.5, \"feet\": 0.3, \"mile\": 1.6, \"pound\": 0.5}",
             scope: "world",
             config: false,
             restricted: true,
